213db72bc13e23cb40bf74301ee1bf1c3d9e5800
jorgecahui <jorgecahui347@gmail.com>
2025-10-21 01:33:43 -0500

    pom

---
+++ Backend/ms-oficina/src/main/java/com/msoficina/MsOficinaApplication.java
@@ -0,0 +1,13 @@
+package com.msoficina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsOficinaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsOficinaApplication.class, args);
+    }
+
+}
---
+++ Backend/ms-oficina/src/test/java/com/msoficina/MsOficinaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msoficina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsOficinaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
