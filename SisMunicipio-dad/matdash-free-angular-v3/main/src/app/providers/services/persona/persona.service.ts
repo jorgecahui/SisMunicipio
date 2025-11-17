9ae3c6ec974f79a76d12c943e9ca506a713a5298
alexanderhuahuacccapacc <alexander123huahuaccapa@gmail.com>
2025-11-17 12:11:37 -0500

    crear,per

--- SisMunicipio-dad/matdash-free-angular-v3/main/src/app/providers/services/persona/persona.service.ts
+++ SisMunicipio-dad/matdash-free-angular-v3/main/src/app/providers/services/persona/persona.service.ts
@@ -2,11 +2,29 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { END_POINTS } from '../../utils/end-points';
 import { EntityDataService } from '../../utils/entity-data';
-import { Persona } from '../../../models/persona.model';
+import {environment} from "../../../../environments/environment";
 
 @Injectable({ providedIn: 'root' })
-export class PersonaService extends EntityDataService<Persona[]> {
-  constructor(protected override httpClient: HttpClient) {
-    super(httpClient, END_POINTS.personas, END_POINTS.personas);
+export class PersonaService extends EntityDataService<any> {
+  constructor(http: HttpClient) {
+
+    super(
+      http,
+
+      // GET ALL (listado general)
+      `${environment.url}${END_POINTS.personas}`,
+
+      // GET BY ID (usa el mismo endpoint)
+      `${environment.url}${END_POINTS.persons_id}`,
+
+      // DELETE
+      `${environment.url}${END_POINTS.persons_delet}`,
+
+      `${environment.url}${END_POINTS.persons_post}`,
+      //`${environment.url}${END_POINTS.camposestraidos_editar}`,
+
+
+    );
+
   }
 }
--- SisMunicipio-dad/matdash-free-angular-v3/main/src/app/providers/utils/end-points.ts
+++ SisMunicipio-dad/matdash-free-angular-v3/main/src/app/providers/utils/end-points.ts
@@ -4,5 +4,10 @@ export const END_POINTS = {
   camposextraidos_listar: '/ocr/listar',
   camposextraidos_eliminar: '/ocr/eliminar',
   camposestraidos_editar: '/ocr/actualizar',
-  camposetraidos_crear: '/ocr/convertir'
+  camposetraidos_crear: '/ocr/convertir',
+  persons_id: '/personas/id',
+  //persons_put: '/personas/id',
+  persons_post: '/personas/id',
+  persons_delet: '/personas/id',
+
 };
