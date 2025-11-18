5a15cbf3cad311eac5cd48839a2e88a2d4164c67
alexanderhuahuacccapacc <alexander123huahuaccapa@gmail.com>
2025-11-18 14:14:03 -0500

    holaaa_dto_user_person

--- SisMunicipio-dad/Backend/ms-auth/src/main/java/com/msauth/dto/PersonaDTO.java
+++ SisMunicipio-dad/Backend/ms-auth/src/main/java/com/msauth/dto/PersonaDTO.java
@@ -2,4 +2,7 @@ package com.msauth.dto;
 
 public class PersonaDTO {
     private Long id;
+
+    public PersonaDTO(Long id, String desconocido, String sinDatos, String number, String s, String s1) {
+    }
 }
