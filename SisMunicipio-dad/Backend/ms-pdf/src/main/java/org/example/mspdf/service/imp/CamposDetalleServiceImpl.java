b5871fcde036cbcd7a1f61bc1e8ea8b745d5e079
alexanderhuahuacccapacc <alexander123huahuaccapa@gmail.com>
2025-11-23 03:46:53 -0500

    lunes17

---
+++ SisMunicipio-dad/Backend/ms-pdf/src/main/java/org/example/mspdf/service/imp/CamposDetalleServiceImpl.java
@@ -0,0 +1,37 @@
+package org.example.mspdf.service.imp;
+
+import org.example.mspdf.entity.CamposDetalle;
+import org.example.mspdf.entity.CamposExtraidos;
+import org.example.mspdf.repository.CamposDetalleRepository;
+import org.example.mspdf.service.CamposDetalleService;
+
+import java.util.List;
+import java.util.Map;
+
+public class CamposDetalleServiceImpl implements CamposDetalleService {
+
+    private final CamposDetalleRepository repository;
+
+    public CamposDetalleServiceImpl(CamposDetalleRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public void guardarDetalles(Map<String, String> campos, CamposExtraidos padre) {
+        campos.forEach((key, value) -> {
+            CamposDetalle det = new CamposDetalle();
+            det.setCampo(key);
+            det.setValor(value);
+            det.setCamposExtraidos(padre);
+
+            repository.save(det);
+        });
+    }
+
+    @Override
+    public List<CamposDetalle> obtenerDetallesPorDocumento(Long id) {
+        CamposExtraidos fake = new CamposExtraidos();
+        fake.setId(id);
+        return repository.findByCamposExtraidos(fake);
+    }
+}
